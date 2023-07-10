9a4f446532526c44b4cc648257953ef0301c32f5
Jhones <jhonesaly@gmail.com>
2023-07-10 16:31:41 -0300

    feat: post name display

--- app_blog/models.py
+++ app_blog/models.py
@@ -8,3 +8,6 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.PROTECT)
     created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
