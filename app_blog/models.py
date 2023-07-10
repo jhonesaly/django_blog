6ba0b3dbaa4c7495e7f8cdf8ce9e905b9af120e9
Jhones <jhonesaly@gmail.com>
2023-07-10 16:08:48 -0300

    feat: post model

--- app_blog/models.py
+++ app_blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    brief = models.CharField(max_length=140)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
