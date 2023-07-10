7203ffbf3b73709955ecafd6deba01a105862078
Jhones <jhonesaly@gmail.com>
2023-07-10 19:49:01 -0300

    feat: views para home e post

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from app_blog.models import Post
+from django.urls import reverse
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.all()
+    url_home = reverse('home')
+    return render(request, 'index.html', {'posts': posts})
+
+def post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    return render(request, 'post.html', {'post': post})
