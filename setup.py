c1228ef4d82953942260a66ce871064572c2ea76
Jhones <jhonesaly@gmail.com>
2023-07-11 15:28:54 -0300

    feat: release setup

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup, find_packages
+import os
+
+def get_gitignore_patterns():
+    gitignore_file = os.path.join(os.path.dirname(__file__), '.gitignore')
+    patterns = []
+
+    with open(gitignore_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                patterns.append(line)
+
+    return patterns
 
 with open('requirements.txt') as req:
     requirements = req.read().splitlines()
 
 setup(
     name='django_blog',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=requirements,
+    exclude=get_gitignore_patterns(),
 )
