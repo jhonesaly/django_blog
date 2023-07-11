0401a8d538216b1084d3913be0e291762deef157
Jhones <jhonesaly@gmail.com>
2023-07-11 13:32:06 -0300

    refactor: simplificação do model dos comentários

--- app_blog/models.py
+++ app_blog/models.py
@@ -13,15 +13,10 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
     name = models.CharField(max_length=255, blank=False)
     comment = models.TextField()
-    STATUS = (
-        (0, 'Filtrado'),
-        (1, 'Permitido'),
-    )
-    status = models.CharField(choices=STATUS, max_length=10, default='Filtrado')
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
