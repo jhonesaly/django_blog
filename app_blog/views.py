ece1ba11592503e5ce9f021af6273eedb18d0d5c
Jhones <jhonesaly@gmail.com>
2023-07-10 22:09:32 -0300

    feat: view do comentário

--- app_blog/views.py
+++ app_blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from app_blog.models import Post
-from django.urls import reverse
+from django.shortcuts import render, HttpResponseRedirect
+from app_blog.models import Post, Comment
+from app_blog.forms import CommentForm
 
 # Create your views here.
 def home(request):
@@ -10,3 +10,16 @@ def home(request):
 def post(request, post_id):
     post = Post.objects.get(pk=post_id)
     return render(request, 'post.html', {'post': post})
+
+def addcomment(request,id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            data = Comment()
+            data.name = form.cleaned_data['name']
+            data.comment = form.cleaned_data['comment']
+            data.post_id = id
+            data.save()
+            return HttpResponseRedirect(url)
+    return HttpResponseRedirect(url)    
