44786b980bd776382e17afc1208868f83c622bca
Jhones <jhonesaly@gmail.com>
2023-07-10 22:10:34 -0300

    feat: comentário na área do admin

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from app_blog.models import Post
+from app_blog.models import Post, Comment
 
 # Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'status']
+    list_filter = ['status']
+
 admin.site.register(Post)
+admin.site.register(Comment, CommentAdmin)
