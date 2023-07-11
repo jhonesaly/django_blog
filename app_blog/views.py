1e9a7ca7ba8cd6376bef493d1f01891a4b73518b
Jhones <jhonesaly@gmail.com>
2023-07-11 13:33:16 -0300

    fix: url router

--- app_blog/views.py
+++ app_blog/views.py
@@ -9,9 +9,10 @@ def home(request):
 
 def post(request, post_id):
     post = Post.objects.get(pk=post_id)
-    return render(request, 'post.html', {'post': post})
+    form = CommentForm()
+    return render(request, 'post.html', {'post': post, 'form':form})
 
-def addcomment(request,id):
+def addcomment(request, post_id):
     url = request.META.get('HTTP_REFERER')
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -19,7 +20,7 @@ def addcomment(request,id):
             data = Comment()
             data.name = form.cleaned_data['name']
             data.comment = form.cleaned_data['comment']
-            data.post_id = id
+            data.post_id = post_id
             data.save()
             return HttpResponseRedirect(url)
-    return HttpResponseRedirect(url)    
+    return HttpResponseRedirect(url)
